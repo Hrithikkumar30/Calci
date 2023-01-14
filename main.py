661f5e750d28d341fb51e5a0996fef0e2f4ddba7
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-01-14 16:20:22 +0530

    calculator done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 a = int(input("enter any number"))
 b = int(input("enter any number"))
 
-c = int(input("enter the method"))
+c = int(input("enter the operator"))
+
+if c == 1:
+    print(a+b)
+if c==2:
+    print(a*b)
+if c==3:
+    print(a-b)
+if c==4:
+    print(a/b)
+else:
+    print("no operation")
